ab84a0ef34e93c937d10acf0deadc9c8e5d232d0
happiness <realcoozoo@gmail.com>
2021-12-04 11:09:06 +0200

    rm

--- initvar.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $iconfile="img/normal/bartheadtiny.png";
-
-///////////// testrail settings /////////////
-$testrailhost='https://testrail.anzogroup.com/';
-$testrailuser='sys_onseo_testrail@onseo.biz';
-$testrailpass='Test.1234';
-/////////////////////////////////////////////
-?>
